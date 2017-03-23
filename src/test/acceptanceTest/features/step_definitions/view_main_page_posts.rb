5c1f408ef0d826c5a57c89ae202fc16d8a1e940d
Kevin Weichel <kevinjweichel@gmail.com>
2017-03-23 09:50:47 -0400

    fleshed out date order check for ruby

--- src/test/acceptanceTest/features/step_definitions/view_main_page_posts.rb
+++ src/test/acceptanceTest/features/step_definitions/view_main_page_posts.rb
@@ -17,5 +17,5 @@ end
 Then(/^then I should see a summary of my favorite blogger's (\d+) most recent posts in reverse order$/) do |arg|
   blog_count = get_blog_count_from_page
   expect(blog_count).to eq 10
-
+  expect(post_dates_are_reverse_order).to eq true
 end
